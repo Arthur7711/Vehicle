e2266a4c9c38c6a17cb6a4fa482336b34110156d
Arthur <newartroll11@gmail.com>
2022-02-18 14:07:22 +0400

    feat:(menu active element selecting)

--- logic.js
+++ logic.js
@@ -1,3 +1,14 @@
+// menu close part
 document.getElementById("closeMenu").addEventListener("click", () => {
   console.log("clicked");
+});
+
+// menu item selecting
+document.querySelectorAll(".menuItem").forEach((el, i) => {
+  el.addEventListener("click", () => {
+    document
+      .querySelectorAll(".menuItem")
+      .forEach((elem) => elem.classList.remove("activeItem"));
+    el.classList.add("activeItem");
+  });
 });
