8b8454de6e27c289fa5387c96d283fe62bd25927
Arthur Ghazaryan <newartroll11@gmail.com>
2022-02-20 14:27:21 +0400

    feat:(opening menu)

--- logic.js
+++ logic.js
@@ -6,6 +6,16 @@ document.getElementById("closeMenu").addEventListener("click", () => {
   document.querySelector(".navigate").style.width = "50px";
 });
 
+// menu open part
+document.getElementById("openMenu").addEventListener("click", () => {
+  document
+    .querySelectorAll(".sp")
+    .forEach((el) => (el.style.display = "block"));
+  document.querySelector(".openMenu").style.display = "none";
+  document.querySelector(".closeMenu").style.display = "flex";
+  document.querySelector(".navigate").style.width = "200px";
+});
+
 // menu item selecting
 document.querySelectorAll(".menuItem").forEach((el, i) => {
   el.addEventListener("click", () => {
